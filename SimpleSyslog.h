2f68da1229d191b3cf0ef8287b2fb330a6adf35b
Domenico Silletti <domenico@silletti.it>
2023-08-20 23:27:30 +0200

    Update SimpleSyslog.h

--- SimpleSyslog.h
+++ SimpleSyslog.h
@@ -59,15 +59,19 @@ class SimpleSyslog {
 			vsnprintf(buf, 128, format, args);
 			va_end(args);
 
-			char time_buf[32] = "-";
+			char time_buf[32];
 			struct tm timeinfo;
   			if(getLocalTime(&timeinfo)) {
-				strftime(time_buf, sizeof(time_buf), "%Y-%m-%dT%H:%M:%SZ%z", &timeinfo));
+				strftime(time_buf, sizeof(time_buf), "%Y-%m-%dT%H:%M:%SZ%z", &timeinfo);
+			}
+			else {
+				snprintf(time_buf, sizeof(time_buf), "-");
 			}
 
 			// This is a unit8 instead of a char because that's what udp.write() wants
 			uint8_t buffer[MAX_PACKET_SIZE];
 			int len = snprintf((char*)buffer, MAX_PACKET_SIZE, "<%d>1 %s %s %s - - - %s", priority, time_buf, _hostname, _app, buf);
+			//IETF RFC5424 FORMAT: <priority>version timestamp hostname application process_id structured_data message_id message"
 
 			// Send the raw UDP packet
 			SimpleSyslog_udp.beginPacket(_server, _port);
